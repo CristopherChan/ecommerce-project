76239e1d76948780e2338cf2290569a53d9fc2e1
CristopherChan <145260110+CristopherChan@users.noreply.github.com>
2024-01-20 16:55:29 +0800

    malapit na

--- search.php
+++ search.php
@@ -8,7 +8,7 @@ include_once("connection.php");
   if(isset($_POST['find'])){
   $search = $_POST['search'];
   
-  $query = $conn->prepare("SELECT * FROM product_tbl WHERE productName LIKE '%$search%' ");
+  $query = $conn->prepare("SELECT * FROM product WHERE product_name LIKE '%$search%' ");
 	$query->execute();
 	
   $count = $query->rowCount();
@@ -24,16 +24,20 @@ include_once("connection.php");
 
   
 			while($row = $query->fetch()){
-				$productName = $row['productName'];
-				$price = $row['productPrice'];
-        
+        $product_image = $row['product_image'];
+				$productName = $row['product_name'];
+				$product_discrip = $row['produc_discrip'];
+        $price = $row['price'];
+
+    
         echo "<table border>
         <tr>
+        <td> <img src="upload/$product_image></td>
         <td>$productName</td>
+        <td>$product_discrip</td>
         <td>$price</td>
         </tr>
         </table>";
-          
       }				
 }
 ?>
